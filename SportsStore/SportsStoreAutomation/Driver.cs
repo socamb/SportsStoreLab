4397d079bffffce4b95ffaddc4572a5c77c7db4a
Scott Ocamb <scott.ocamb@roadmaptoagile.com>
2017-07-31 14:43:38 -0400

    First UI Selenium UI Tests

---
+++ SportsStore/SportsStoreAutomation/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SportsStoreAutomation
+{
+
+    // There will be a driver for each browser type.
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; } 
+
+        public static void Initalize()
+        {
+            Instance = new FirefoxDriver();
+            Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+        public static void Close()
+        {
+            Instance.Close();
+        }
+    }
+}
---
+++ SportsStore/SportsStoreAutomation/MaintenancePage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SportsStoreAutomation
+{
+    public class MaintenancePage
+    {
+
+        public static string AllProducts
+        {
+            get
+            {
+
+                // Still expermenting how to do this best. For now it is done this way.
+                try
+                {
+                    var AddProduct = Driver.Instance.FindElement(By.Id("Add-New-Product"));
+                    var h3s = Driver.Instance.FindElements(By.TagName("h3"));
+                    return h3s[0].Text;
+                }
+                catch (OpenQA.Selenium.NoSuchElementException e)
+                {
+                    return "NoSuchElementExteproion - Login Error";
+                }
+   
+            }
+
+        }
+
+
+
+
+    }
+}
