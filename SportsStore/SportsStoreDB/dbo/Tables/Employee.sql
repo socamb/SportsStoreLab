f441611711528e0b4c2eb886a3096c3394347e5d
Scott Ocamb <scott.ocamb@roadmaptoagile.com>
2018-01-08 09:21:03 -0500

    Add Last Name

--- SportsStore/SportsStoreDB/dbo/Tables/Employee.sql
+++ SportsStore/SportsStoreDB/dbo/Tables/Employee.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
   [EmployeeId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] NVARCHAR(100) NOT NULL
+    [FirstName] NVARCHAR(100) NOT NULL, 
+    [LastName] NVARCHAR(100) NULL
 )
