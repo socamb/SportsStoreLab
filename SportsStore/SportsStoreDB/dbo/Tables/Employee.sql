f6b9ef83845228e5c827addb5f961074645cd640
Scott Ocamb <scott.ocamb@roadmaptoagile.com>
2018-01-08 09:26:55 -0500

    Add City

--- SportsStore/SportsStoreDB/dbo/Tables/Employee.sql
+++ SportsStore/SportsStoreDB/dbo/Tables/Employee.sql
@@ -2,5 +2,6 @@
 (
   [EmployeeId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] NVARCHAR(100) NOT NULL, 
-    [LastName] NVARCHAR(100) NULL
+    [LastName] NVARCHAR(100) NULL, 
+    [City] NVARCHAR(50) NULL
 )
